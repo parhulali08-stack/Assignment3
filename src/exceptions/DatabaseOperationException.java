7c3a9988d1d01b710cd1445439e2292290c35a5e
parhulali08-stack <tarhul.ali08@gmail.com>
2026-02-04 21:35:03 +0500

    Change DatabaseOperationException to extend RuntimeException

--- src/exceptions/DatabaseOperationException.java
+++ src/exceptions/DatabaseOperationException.java
@@ -1,7 +1,11 @@
 package exceptions;
 
-public class DatabaseOperationException extends Exception {
+public class DatabaseOperationException extends RuntimeException {
     public DatabaseOperationException(String message) {
         super(message);
+    }
+    
+    public DatabaseOperationException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
