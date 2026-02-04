0fe922135dac76e328b20409f5fb4ed5c62b4a67
parhulali08-stack <tarhul.ali08@gmail.com>
2026-02-04 21:30:07 +0500

    Add default method displayWithHeader to Displayable
    Added a default method to display with a header.

--- src/interfaces/Displayable.java
+++ src/interfaces/Displayable.java
@@ -1,5 +1,11 @@
 package interfaces;
 
+@FunctionalInterface
 public interface Displayable {
     void display();
+    
+    default void displayWithHeader(String header) {
+        System.out.println("=== " + header + " ===");
+        display();
+    }
 }
