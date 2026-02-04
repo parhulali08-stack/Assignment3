20381874ca16ad27334dcac8fe5bbc534e7b04cc
parhulali08-stack <tarhul.ali08@gmail.com>
2026-02-04 21:36:34 +0500

    Update database connection URL to Assignment3

--- src/database/DatabaseConnection.java
+++ src/database/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/library_db";
+    private static final String URL = "jdbc:postgresql://localhost:5432/Assignment3";
     private static final String USER = "postgres";
     private static final String PASSWORD = "password";
     private static Connection connection;
